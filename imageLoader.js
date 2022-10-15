1b23a1457d4210df3207a6ce1f6b348f6dc33437
Viktor Slavchev <viki_slavchev@abv.bg>
2022-10-15 12:45:09 +0300

    AS

--- imageLoader.js
+++ imageLoader.js
@@ -1,21 +1,32 @@
-function loadImg(src){
-    result = {};
-    result.img = new Image();
-    result.img.src = src;
-    return result;
+function loadImg(src) {
+	result = {};
+	result.img = new Image();
+	result.img.src = src;
+	return result;
 }
-function rect(c, x, y, sx, sy){
-    context.save();
-    context.fillStyle = c;
-    context.translate(transX, transY);
-    context.fillRect(x*screenRatio, y*screenRatio, sx*screenRatio, sy*screenRatio);
-    context.restore();
+function rect(c, x, y, sx, sy) {
+	context.save();
+	context.fillStyle = c;
+	context.translate(transX, transY);
+	context.fillRect(
+		x * screenRatio,
+		y * screenRatio,
+		sx * screenRatio,
+		sy * screenRatio
+	);
+	context.restore();
 }
-function drawImage(img, x, y, xs, ys){
-    context.save();
-    context.translate(transX, transY);
-    context.drawImage(img.img, x*screenRatio, y*screenRatio, xs*screenRatio, ys*screenRatio);
-    context.restore();
+function drawImage(img, x, y, xs, ys) {
+	context.save();
+	context.translate(transX, transY);
+	context.drawImage(
+		img.img,
+		x * screenRatio,
+		y * screenRatio,
+		xs * screenRatio,
+		ys * screenRatio
+	);
+	context.restore();
 }
 let circle = loadImg("./images/circle.png");
 let square = loadImg("./images/square.png");
--- setup.js
+++ setup.js
@@ -1,12 +1,21 @@
-let canvas = document.getElementById("canvas"), mx = 0, my = 0;
+let canvas = document.getElementById("canvas"),
+	mx = 0,
+	my = 0;
 let context = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 const gamingState = 0;
 const mainState = 1;
-const fixedScreenSize = {x: 1000, y: 500};
+const fixedScreenSize = { x: 1000, y: 500 };
 let State = gamingState;
-const generalScale = Math.min((canvas.height-10)/(fixedScreenSize.y), (canvas.width-10)/(fixedScreenSize.x));
-const ScreenSize = {x: fixedScreenSize.x*generalScale, y: fixedScreenSize.y*generalScale};
-const transX = (canvas.width-ScreenSize.x)/2, transY = (canvas.height-ScreenSize.y)/2;
-const screenRatio = ScreenSize.x/fixedScreenSize.x;
+const generalScale = Math.min(
+	(canvas.height - 10) / fixedScreenSize.y,
+	(canvas.width - 10) / fixedScreenSize.x
+);
+const ScreenSize = {
+	x: fixedScreenSize.x * generalScale,
+	y: fixedScreenSize.y * generalScale,
+};
+const transX = (canvas.width - ScreenSize.x) / 2,
+	transY = (canvas.height - ScreenSize.y) / 2;
+const screenRatio = ScreenSize.x / fixedScreenSize.x;
