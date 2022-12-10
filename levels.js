181a4a2570603fd885fd24e73a2994028f016886
unknown <krischanpower@gmail.com>
2022-12-10 11:17:20 +0200

    inventory and req

--- levels.js
+++ levels.js
@@ -1,31 +1,34 @@
-const types = ["door", "wall", "wall!", "mebel", "gold"];
+const types = ["brown", "gray", "cyan", "red", "yellow"];
 class Box {
-	constructor(x, y, sx, sy, type) {
+	constructor(x, y, sx, sy, color, req) {
 		this.x = x;
 		this.y = y;
 		this.sx = sx;
 		this.sy = sy;
-		this.type = type;
+		this.color = color;
+		this.req = req;
 	}
 	draw() {
-		let color;
-		if (this.type == types[0]) {
-			color = "brown";
-		} else if (this.type == types[1]) {
-			color = "gray";
-		} else if (this.type == types[2]) {
-			color = "lightblue";
-		} else if (this.type == types[3]) {
-			color = "red";
-		} else if (this.type == types[4]) {
-			color = "yellow";
-		}
-		rect(color, this.x * 1, this.y * 1, this.sx * 1, this.sy * 1);
+		// let color;
+		// if (this.type == types[0]) {
+		// 	color = "brown";
+		// } else if (this.type == types[1]) {
+		// 	color = "gray";
+		// } else if (this.type == types[2]) {
+		// 	color = "lightblue";
+		// } else if (this.type == types[3]) {
+		// 	color = "red";
+		// } else if (this.type == types[4]) {
+		// 	color = "yellow";
+		// }
+		// context.fillStyle = this.color;
+		rect(this.color, this.x * 1, this.y * 1, this.sx * 1, this.sy * 1);
 	}
 }
 class Item{
-	constructor(texture){
+	constructor(texture, type){//types - key, gold
 		this.texture = texture;
+		this.type = type;
 	}
 }
 class Level{
@@ -37,7 +40,7 @@ class Level{
 }
 function editLevel(lvl, tx, ty, scalar){
     for(let i=0; i<lvl.length; i++){
-        lvl[i] = new Box(lvl[i].x*scalar+tx, lvl[i].y*scalar+ty, lvl[i].sx*scalar, lvl[i].sy*scalar, lvl[i].type);
+        lvl[i] = new Box(lvl[i].x*scalar+tx, lvl[i].y*scalar+ty, lvl[i].sx*scalar, lvl[i].sy*scalar, lvl[i].color, lvl[i].req);
     }
     return lvl;
 }
